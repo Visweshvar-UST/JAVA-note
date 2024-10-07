3ff0dc57b819fc87986220d0627e36f5fe4dccba
Visweshvar-UST <287618@ust.com>
2024-10-07 17:59:51 +0530

    contest

--- src/main/java/org/example/functionalinterface/Contest_1.java
+++ src/main/java/org/example/functionalinterface/Contest_1.java
@@ -10,5 +10,26 @@ public class Contest_1 {
         Function<Integer, String> isEven = num -> (num%2==0)?"Even":"Odd";
         System.out.println(oddEven.test(2)?"Even":"Odd");
 
+        Predicate<Integer> isPrime = (num) -> {
+            for(int i = 2; i*i < num; i++){
+                if(num%i==0) return false;
+            }
+            return true;
+        };
+        System.out.println(isPrime.test(5)?"Prime":"Not Prime");
+
+        Predicate<String> isPalindrome = (str) -> {
+            char[] inp = str.toCharArray();
+            int l = inp.length;
+            for (int i = 0; i < l/2; i++){
+                if (inp[i] != inp[l-1-i]) return false;
+            }
+            return true;
+        };
+        System.out.println(isPalindrome.test("madam")?"Palindrome":"Not Palindrome");
+
+
+
+
     }
 }
