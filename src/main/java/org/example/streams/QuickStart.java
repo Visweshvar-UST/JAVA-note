8270d76bd73586129ae8f35274cca6f12f6b1996
Visweshvar-UST <287618@ust.com>
2024-10-08 16:21:02 +0530

    streams

--- src/main/java/org/example/streams/QuickStart.java
+++ src/main/java/org/example/streams/QuickStart.java
@@ -1,7 +1,17 @@
 package org.example.streams;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickStart {
     public static void main(String[] args) {
+        List<String> inp = new ArrayList<>();
+        inp.add("viswa");
+        inp.add("axel");
+        List<String> res = inp.stream().filter(obj -> obj.contains("v") || obj.contains("V")).toList();
+        System.out.println(res);
 
     }
 }
+
